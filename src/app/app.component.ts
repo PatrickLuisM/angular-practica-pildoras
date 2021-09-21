8664c563b3076bf2ef776fae3e31b099c4dfbb3c
Patrick Luis <patricklmo32@gmail.com>
2021-09-20 19:28:08 -0500

    practica-pildoras

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'miniCalculadora';
+  title = 'Calculadora Mini';
+
+  numero1: number = 0;
+  numero2: number = 0;
+  resultado: number = 0;
+
+  sumar():number{
+    this.resultado = Number(this.numero1) + Number(this.numero2);
+    return this.resultado;
+  }
+
+  restar():number{
+    this.resultado = Number(this.numero1) - Number(this.numero2);
+    return this.resultado;
+  }
+
+  multiplicar():number{
+    this.resultado = Number(this.numero1) * Number(this.numero2);
+    return this.resultado;
+  }
+
+  dividir():number{
+    this.resultado = Number(this.numero1) / Number(this.numero2);
+    return this.resultado;
+  }
 }
